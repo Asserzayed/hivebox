428e7f216e719dd94503fd28f0d94617ca1514fc
Asser Zayed <asserzayed@gmail.com>
2024-12-17 00:18:23 +0200

    feat: print help if no args are passed

--- main.py
+++ main.py
@@ -7,15 +7,25 @@ TOOL_VERSION = "0.0.1"
 
 def main():
     # Initialize argument parser
-    parser = argparse.ArgumentParser(description="HiveBox DevOps CLI tool.")
+    parser = argparse.ArgumentParser(description="HiveBox DevOps CLI tool.",
+                                    epilog="Use '%(prog)s <command> --help' to get help on a specific command.")
     
     # Add the --version flag
-    parser.add_argument("--version", "-v", action="version", version=f"HiveBox v{TOOL_VERSION}", help="Shows tool version.")
+    parser.add_argument("--version", "-v",
+                        action="version",
+                        version=f"HiveBox v{TOOL_VERSION}",
+                        help="Shows tool version.")
 
     # Parse arguments
     args = parser.parse_args()
-    # Show logic route # Testing
+    
+    # Testing #
+    # Show logic route
     # print(args)
+
+    # Print help if no args are passed
+    if not any(vars(args).values()):
+        parser.print_help()
 
 if __name__ == "__main__":
     main()
